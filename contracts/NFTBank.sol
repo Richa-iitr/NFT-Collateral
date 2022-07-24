64714090c8f42b11e86cf7e7bc90569c43d68c47
Richa-iitr <richa@cs.iitr.ac.in>
2022-07-24 18:01:37 +0530

    linting

--- contracts/NFTBank.sol
+++ contracts/NFTBank.sol
@@ -22,12 +22,20 @@ abstract contract NFTEscrow is ERC1155, IERC721Receiver {
             interfaceId == type(IERC1155Receiver).interfaceId;
     }
 
+    /**
+     * @dev returns the token id of the NFT corresponding to the details passed as param.
+     * @param nftContract address of the NFT contract.
+     * @param nftId token id of the borrower's NFT.
+     * @param endTime the end date for the loan.
+     * @param debtCeiling the debt ceiling of the loan expected by the borrower.
+     * @param interestRate expected interest rate of the loan by the borrower.
+     */
     function getTokenId(
         address nftContract,
         uint256 nftId,
         uint256 endTime,
-        uint256 borrowCeiling,
-        uint256 interest
+        uint256 debtCeiling,
+        uint256 interestRate
     ) public pure returns (uint256 id) {
         return
             uint256(
@@ -36,13 +44,22 @@ abstract contract NFTEscrow is ERC1155, IERC721Receiver {
                         nftContract,
                         nftId,
                         endTime,
-                        borrowCeiling,
-                        interest
+                        debtCeiling,
+                        interestRate
                     )
                 )
             );
     }
 
+    /**
+     * @dev creates an NFT with details of the loan and assigns them to borrower until a loan offer is accepted.
+     * @param owner NFT owner
+     * @param nftContract address of the NFT
+     * @param nftId NFT id
+     * @param endTime the time of the loan repayment
+     * @param debtCeiling debt ceiling set by the borrower 
+     * @param interestRate the rate of the loan
+     */
     function create(
         address owner,
         address nftContract,
@@ -63,6 +80,16 @@ abstract contract NFTEscrow is ERC1155, IERC721Receiver {
         _mint(owner, id, 1, "");
     }
 
+    /**
+     * @dev id for lender's NFT 
+     * @param id previous id
+     */
+    function lenderTokenId(uint256 id) internal pure returns (uint256) {
+        unchecked {
+            return id + 1;
+        }
+    }
+
     function onERC721Received(
         address _operator,
         address _from,
@@ -81,8 +108,20 @@ abstract contract NFTEscrow is ERC1155, IERC721Receiver {
         );
         return this.onERC721Received.selector;
     }
+
+    function onERC1155Received(
+        address _operator,
+        address _from,
+        uint256 _id,
+        uint256 _value,
+        bytes calldata _data
+    ) external returns (bytes4) {
+        require(_value == 1);
+        onERC721Received(_operator, _from, _id, _data);
+        return this.onERC1155Received.selector;
+    }
 }
 
-contract NFTBank is NFTEscrow {
-    string public name = "nft-bank-v1.0";
+contract NFTCollateral is NFTEscrow {
+    string public name = "nft-collateral-v1.0";
 }
